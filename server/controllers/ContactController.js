c81bdbd34eeae853b44a5fe8779f1d9c5839a585
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2019-06-15 11:22:07 +0100

    add register contact controller

--- server/controllers/ContactController.js
+++ server/controllers/ContactController.js
@@ -1,6 +1,39 @@
+import {STATUS_CODE} from 'http';
+import contactDao from '../database/dao/contact';
+
 class ContactController {
-  static add(req, res) {
-    return res.send('contact add');
+  static async add(req, res) {
+    let {name, phoneNumber} = this.body;
+    phoneNumber = parseInt(phoneNumber);
+
+    if (!name.length) {
+      return res
+        .status(402)
+        .json({status: STATUS_CODE[402], message: 'Contact name is required.'});
+    }
+    if (isNaN(phoneNumber) || phoneNumber.split('').length != 10) {
+      return res.status(402).json({
+        status: STATUS_CODE[402],
+        message: 'Phone number is required and must be 10 digits',
+      });
+    }
+    try {
+      const existingPhoneNumber = contactDao.findContactByNumber(phoneNumber);
+
+      if (existingPhoneNumber) {
+        return res.status(408).json({
+          status: STATUS_CODE[408],
+          message: 'Phone number has already been registered.',
+        });
+        const contact = await contactDao.add(name, phoneNumber);
+        return res
+          .status(201)
+          .json({status: STATUS_CODE[201], payload: {contact}});
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json('Internal server error.');
+    }
   }
   static delete(req, res) {
     return res.send('contact delete');
